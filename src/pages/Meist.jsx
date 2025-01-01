50aba116ef51f87a8d1baa0b28d51ed6cffd4060
KT147 <kevin.tammm@gmail.com>
2025-01-01 13:28:06 +0200

    klassid boldiks

--- src/pages/Meist.jsx
+++ src/pages/Meist.jsx
@@ -11,20 +11,20 @@ function Meist() {
     <div className="tootajad">Meie töötajad</div>
     <br/>
     <div style={{ display: 'flex', justifyContent: 'space-between', gap: '80px'}}>
-      <div>Liisi Kass
-      <div onClick={() => muudaN2itaTelJuhataja(!n2iteTelJuhataja)}>Juhataja</div>
-      {n2iteTelJuhataja && <div>tel. 53535353</div>}
-      <div>liisi.kass@kingapood.ee</div>
+      <div className={n2iteTelJuhataja === true ? "valitud" : undefined}>Liisi Kass
+      <div className={n2iteTelJuhataja === true ? "valitud" : undefined} onClick={() => muudaN2itaTelJuhataja(!n2iteTelJuhataja)}>Juhataja</div>
+      {n2iteTelJuhataja && <div className="valitud">tel. 53535353</div>}
+      <div className={n2iteTelJuhataja === true ? "valitud" : undefined}>liisi.kass@kingapood.ee</div>
       </div>
-      <div>Anna Käpp
-      <div onClick={() => muudaN2itaTelTurundus(!n2iteTelTurundus)}>Turundusspetsialist</div>
-      {n2iteTelTurundus && <div>tel. 53434343</div>}
-      <div>anna.kapp@kingapood.ee</div>
+      <div className={n2iteTelTurundus === true ? "valitud" : undefined}>Anna Käpp
+      <div className={n2iteTelTurundus === true ? "valitud" : undefined} onClick={() => muudaN2itaTelTurundus(!n2iteTelTurundus)}>Turundusspetsialist</div>
+      {n2iteTelTurundus && <div className="valitud">tel. 53434343</div>}
+      <div className={n2iteTelTurundus === true ? "valitud" : undefined}>anna.kapp@kingapood.ee</div>
       </div>
-      <div>Artul Kartul
-      <div onClick={() => muudaN2itaTelKlient(!n2iteTelKlient)}>Klienditeenidaja</div>
-      {n2iteTelKlient && <div>tel. 53574743</div>}
-      <div>artul.kartul@kingapood.ee</div>
+      <div className={n2iteTelKlient === true ? "valitud" : undefined}>Artul Kartul
+      <div className={n2iteTelKlient === true ? "valitud" : undefined} onClick={() => muudaN2itaTelKlient(!n2iteTelKlient)}>Klienditeenidaja</div>
+      {n2iteTelKlient && <div className="valitud">tel. 53574743</div>}
+      <div className={n2iteTelKlient === true ? "valitud" : undefined}>artul.kartul@kingapood.ee</div>
       </div>
     </div>
 
