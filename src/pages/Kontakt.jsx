95874b80b0e6e1a3d3011e159e736977804d603c
KT147 <kevin.tammm@gmail.com>
2025-01-19 15:39:27 +0200

    .

--- src/pages/Kontakt.jsx
+++ src/pages/Kontakt.jsx
@@ -1,13 +1,22 @@
 import { useState } from "react"
+import tootajadFailist from "./data/tootajad.json"
 
 function Kontakt() {
-  const [kontakt, n2itakontakt] = useState("");
+  const [kontakt, n2itaKontakt] = useState("");
+  const tootajad = tootajadFailist
+  const [valitud, m22ravalitud] = useState("");
+
+  const votaYhendust = (tootaja) => {
+    n2itaKontakt (tootaja.Telefon);
+    m22ravalitud(tootaja.Nimi)
+  }
+
+
   return (
     <div>
-      <div>
         <div>Võta meiega ühendust</div>
         <br /> <br />
-        <div>Liisi Kass</div>
+        {/* <div>Liisi Kass</div>
         <div>Juhataja</div>
         <button onClick={() => n2itakontakt ("52525252")}>Võta ühendust</button>
         <br /> <br />
@@ -18,10 +27,17 @@ function Kontakt() {
         <div>Artul Kartul</div>
         <div>Klienditeenindaja</div>
         <button onClick={() => n2itakontakt ("5464544")}>Võta ühendust</button>
-        <br /><br />
+        <br /><br /> */}
+        VALITUD INIMENE: {valitud}
         { kontakt !== "" && <div>Tema kontakt: {kontakt}</div>}
+        <div>{tootajad.map(tootaja =>
+        <div className={tootaja.Nimi === valitud ? "valitud" : undefined} key={tootaja.Nimi}>
+           <div> {tootaja.Nimi}</div>
+           <div> {tootaja.Ala}</div>
+           <button onClick={() => votaYhendust(tootaja)}>Võta ühendus</button>
+           </div>)}
       </div>
-    </div>
+      </div>
 
   )
 }
